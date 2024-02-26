365c25efa97b846a62ab58b5269ccbe990bd1dc3
Mayur Vijay Bhat <mvbhat@iu.edu>
2024-02-25 20:39:23 -0500

    Adding serializers and views

--- bank_management_backend/app/serializers.py
+++ bank_management_backend/app/serializers.py
@@ -5,3 +5,34 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        print("CreateUserSerializer")
+        model = User
+        fields = ('name', 'user_name', 'email', 'password', 'user_type', 'user_role')
+        # extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(slf, data):
+        # Check if the email is unique
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError("Email already exists")
+        # Check if the user_name is unique
+        if User.objects.filter(user_name=data['user_name']).exists():
+            raise serializers.ValidationError("Username already exists")
+        return data
+
+class LoginUserSerializer(serializers.Serializer):
+        #Check if the user exists
+        user_name = serializers.CharField()
+        password = serializers.CharField()
+        def validate(self, data):
+            user = User.objects.filter(
+                user_name=data["user_name"]
+            )
+            if not user.exists():
+                raise serializers.ValidationError("User not founddd")
+            #Check if ppassword is correct
+            if user.first().password != data["password"]:
+                raise serializers.ValidationError("Password is incorrect")
+            return data
--- bank_management_backend/app/views.py
+++ bank_management_backend/app/views.py
@@ -1,8 +1,60 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, CreateUserSerializer, LoginUserSerializer
 # Create your views here.
+
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer 
+
+
+class CreateUserView(APIView):
+    print("CreateUserView")
+    serializer_class = CreateUserSerializer
+
+    def post(self, request):
+        print(request.data)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            print("Validated", serializer.validated_data)
+            user = User(
+                name=serializer.validated_data["name"],
+                user_name=serializer.validated_data["user_name"],
+                email=serializer.validated_data["email"],
+                password=serializer.validated_data["password"],
+                user_type=serializer.validated_data["user_type"],
+                user_role=serializer.validated_data["user_role"]
+            )
+            user.save()
+            print("User saved")
+            print("User_Data", UserSerializer(user).data)
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        print("Invalid data", serializer.errors)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={"data": serializer.errors})
+
+class LoginUserView(APIView):
+    serializer_class = LoginUserSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            # If validation passes, retrieve the user.
+            user = User.objects.get(user_name=serializer.validated_data["user_name"])
+            user_role = user.user_role
+            # Implement session login logic.
+            
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
+            print("Invalid data", serializer.errors)
+            # If validation fails, return the error messages.
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+# This will return a list of books
+@api_view(["GET"])
+def book(request):
+    books = ["Pro Python", "Fluent Python", "Speaking javascript", "The Go programming language"]
+    return Response(status=status.HTTP_200_OK, data={"data": books})
